74e5eda16625dc6425f906b4053f58f4c75b7578
sillybilly <123352677+0arbyzick0@users.noreply.github.com>
2025-03-03 13:46:22 +0700

    обновил библиотеку, потом доделаю буквы, отриц. числа, и анимации

--- quaddisplay.py
+++ quaddisplay.py
@@ -11,7 +11,7 @@ class QuadDisplay:
         self.segments = [Pin(pin, Pin.OUT) for pin in segment_pins]
         self.digit_pins = [Pin(pin, Pin.OUT) for pin in digit_pins]
         
-        # Паттерны для цифр 0-9 и пустой клетки
+        # Паттерны для цифр 0-9, пустой клетки и точки
         self.digits = [
             [1, 1, 1, 1, 1, 0, 1, 0],  # 0
             [0, 1, 1, 0, 0, 0, 0, 0],  # 1
@@ -23,13 +23,14 @@ class QuadDisplay:
             [1, 1, 1, 0, 0, 0, 0, 0],  # 7
             [1, 1, 1, 1, 1, 1, 1, 0],  # 8
             [1, 1, 1, 1, 0, 1, 1, 0],  # 9
-            [0, 0, 0, 0, 0, 0, 0, 0]   # Пустая клетка
+            [0, 0, 0, 0, 0, 0, 0, 0],  # Пустая клетка
+            [0, 0, 0, 0, 0, 0, 0, 1]   # Точка
         ]
 
     def display_digit(self, num, position):
         """
-        Отображает одну цифру на указанном разряде.
-        :param num: Цифра для отображения (0-9).
+        Отображает одну цифру или точку на указанном разряде.
+        :param num: Цифра для отображения (0-9) или 10 для пустой клетки, 11 для точки.
         :param position: Позиция разряда (0-3).
         """
         for pin in self.digit_pins:
@@ -39,22 +40,26 @@ class QuadDisplay:
         for i in range(8):  # Устанавливаем сегменты
             self.segments[i].value(self.digits[num][i])
 
-    def display_number(self, number, leading_zeros=False, duration=1):
+    def number(self, number, zeros=False, duration=1):
         """
-        Отображает 4-значное число на дисплее.
-        :param number: Число для отображения (0-9999).
-        :param leading_zeros: Если True, отображает ведущие нули.
+        Отображает число на дисплее.
+        :param number: Число для отображения (целое или дробное, например, 123 или 12.34).
+        :param zeros: Если True, отображает ведущие нули.
         :param duration: Время отображения числа в секундах.
         """
         start_time = utime.time()  # Запоминаем время начала
+        num_str = "{:04}".format(number) if isinstance(number, int) else "{:05}".format(number)
         while utime.time() - start_time < duration:  # Отображаем число в течение duration секунд
-            num_str = "{:04d}".format(number)
-            for i in range(4):
-                digit = int(num_str[i])
-                if not leading_zeros and digit == 0 and i < 3:  # Пропускаем ведущие нули
-                    self.display_digit(10, i)  # Пустая клетка
+            for i in range(len(num_str)):
+                char = num_str[i]
+                if char == '.':
+                    self.display_digit(11, i)  # Отображаем точку
                 else:
-                    self.display_digit(digit, i)
+                    digit = int(char)
+                    if not zeros and digit == 0 and i < len(num_str) - 1:  # Пропускаем ведущие нули
+                        self.display_digit(10, i)  # Пустая клетка
+                    else:
+                        self.display_digit(digit, i)
                 utime.sleep_ms(5)  # Задержка для мультиплексирования
                 self.clear()  # Очищаем дисплей перед следующим разрядом
 
@@ -64,17 +69,3 @@ class QuadDisplay:
             pin.value(1)  # Выключаем все разряды
         for pin in self.segments:
             pin.value(0)  # Выключаем все сегменты
-
-# Пример использования
-segment_pins = [15, 14, 13, 12, 11, 10, 9, 8]  # Пины для сегментов
-digit_pins = [16, 17, 18, 19]  # Пины для разрядов
-
-display = QuadDisplay(segment_pins, digit_pins)
-
-# Бесконечный цикл для отображения чисел
-while True:
-    # Отображаем число 256 с ведущими нулями в течение 5 секунд
-    display.display_number(256, leading_zeros=True, duration=5)
-    
-    # Отображаем число 257 без ведущих нулей в течение 5 секунд
-    display.display_number(257, leading_zeros=False, duration=5)
